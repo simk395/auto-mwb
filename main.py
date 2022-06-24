06798c22edb8ca814ef9fd9368df491a7634fb1c
Simon Kong <skongnyc@gmail.com>
2022-06-24 15:10:24 -0400

    Added write config file, radio buttons on gui. Now works on free and pt ver.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import json
-import keyboard
 import pyautogui
-import win32gui
+import win32gui, win32com.client
 from tkinter import *
 from tkinter import filedialog
 
 path = None
+version = None
 
 def windowEnumerationHandler(hwnd, top_windows):
     top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
@@ -20,11 +20,7 @@ def getProcess(app_name):
             app = i[0]
             break
     return app
-
-def focusWindow(app):
-    win32gui.ShowWindow(app, 5)
-    win32gui.SetForegroundWindow(app)
-
+  
 def getPath(entry):
     global path
     path = filedialog.askopenfilename(initialdir="/", title="Select file",
@@ -44,9 +40,14 @@ def handleCancel():
     quit()
     
 def createGUI():
+    global version
     win = Tk()
     win.geometry("560x90")
     win.resizable(False, False)
+
+    version = IntVar()
+    version.set(1)
+
     frame = Frame(win)
     frame.pack()
 
@@ -61,6 +62,12 @@ def createGUI():
     browse = Button(frame, text = "Browse", width = 6, command = lambda: getPath(entry))
     browse.grid(row = 1, column = 1)
 
+    free = Radiobutton(frame, text = "Free", variable = version, value = 1)
+    free.grid(row = 2, column = 0, sticky = "w", padx = 5, pady = 5)
+
+    premium = Radiobutton(frame, text = "Premium Trial", variable = version, value = 2)
+    premium.grid(row = 2, column = 0, padx = 5, pady = 5)
+
     confirm = Button(frame, text = "OK", width = 6, command = lambda:handleConfirm(win) )
     confirm.grid(row = 2, column = 0, sticky="e" , padx = 5, pady = 5)
 
@@ -71,45 +78,61 @@ def createGUI():
     win.mainloop()
 
 def openGUI(data, input):
+    global version
     createGUI()
     data["settings"]["location"] = path
     data["settings"]["setup"] = True
+    version = version.get()
+
+    match version:
+        case 1: data["settings"]["presses"] = 11
+        case 2: data["settings"]["presses"] = 15
     input.seek(0)
     json.dump(data, input)
     input.truncate()
 
 def main():
     if __name__ == "__main__":
-        with open('config.json', 'r+') as input:
-            global path
-            data = json.load(input)
-            path = data["settings"]["location"]
+        try:
+            with open('config.json', 'r+') as input:
+                global path, free
+                data = json.load(input)
+                path = data["settings"]["location"]
 
-            if data["settings"]["setup"] == False:
-                openGUI(data, input)
+                if data["settings"]["setup"] == False:
+                    openGUI(data, input)
 
-            if "mbam.exe" in path:
-                try:
+                if "mbam.exe" in path:
                     os.startfile(path)
+                    free = ""
+                    premium = ""
+                    while free == "" and premium == "":
+                        free = getProcess("malwarebytes free")
+                        premium = getProcess("malwarebytes premium trial")
+
+                    fg = None
                     mb = None
-                    found = None
-                    while mb == None or mb == "":
-                        mb = getProcess("malwarebytes free")
+                    shell = win32com.client.Dispatch("WScript.Shell")
+                    if premium == "":
+                        mb = free
+                    else:
+                        mb = premium
+                        
+                    while fg != mb:
+                        win32gui.ShowWindow(mb, 5)
+                        shell.SendKeys('%')
+                        win32gui.SetForegroundWindow(mb)
+                        fg = win32gui.GetForegroundWindow()
                     
-                    while True:
-                        focusWindow(mb)
-                        found = pyautogui.locateCenterOnScreen("scan.png")
-                        if found != None:
-                            break
+                    pyautogui.press("tab" , presses = data["settings"]["presses"], interval = 0.00000001)
+                    pyautogui.press("space")
+                else:
+                    openGUI(data, input)
                     
-                    # move to and click on mwb scan button
-                    pyautogui.moveTo(found[0], found[1])
-                    pyautogui.click()
-                    # minimize mwb window
-                    win32gui.ShowWindow(mb, 2)
-                except TypeError:
-                    print("hello")
-            else:
-                openGUI(data, input)
-        
+        except FileNotFoundError:
+            settings = {"settings": {"setup": False,"presses": 11, "location": "C:/Program Files/Malwarebytes/Anti-Malware/mbam.exe"}}
+            with open('config.json', 'w') as input:
+                json.dump(settings, input)
+            main()
+                                    
 main()
